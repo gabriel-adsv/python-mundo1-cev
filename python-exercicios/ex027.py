ecc71c2c6903d0ac0844410d8bd4a5b09995ab0c
Gabriel Augusto <gabriel.dms@gmail.com>
2024-12-08 23:57:45 -0300

    alteração no exercício 27

--- python-exercicios/ex027.py
+++ python-exercicios/ex027.py
@@ -5,14 +5,14 @@
 #     Primeiro: Ana
 #     Último: Souza
 
-# completo = str(input('Digite o nome completo: ')).strip()
-# nome = completo.split()
-# print('O primeiro nome é: {}'.format(nome[0]))
-# print('O último nome é: {}'.format(nome[-1]))
+completo = str(input('Digite o nome completo: ')).strip()
+nome = completo.split()
+print('O primeiro nome é: {}'.format(nome[0]))
+print('O último nome é: {}'.format(nome[-1]))
 
 # EXERCÍCIO FEITO EM AULA
-n = str(input('Digite seu nome completo: ')).strip()
-nome = n.split()
-print('Muito prazer em te conhecer!')
-print('Seu primeiro nome é {}'.format(nome[0]))
-print('Seu último nome é {}'.format(nome[len(nome) - 1]))
+# n = str(input('Digite seu nome completo: ')).strip()
+# nome = n.split()
+# print('Muito prazer em te conhecer!')
+# print('Seu primeiro nome é {}'.format(nome[0]))
+# print('Seu último nome é {}'.format(nome[len(nome) - 1]))
